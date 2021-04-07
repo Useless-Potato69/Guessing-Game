473e06c4566d346bc90316e7bab90a55f4c436f7
Fredrick Square <>
2021-04-07 22:24:43 +0000

    0.0.0.3 Game now runs, added the main framework game will end if the user runs out of lives or guesses the number correctly
    Commenting added for the most part, a few touch ups are needed
    
    user can now see their guesses both during the game and in the end game screen
    
    The user now gets revealed the number on the end game screen
    
    NEXT:
    Add replay

--- main.py
+++ main.py
@@ -32,41 +32,72 @@ print("You have 3 lives. Goodluck.")
 print("")
 input("Press Enter to start...")
 
+#Game begins
 while bool_playagain is True:
   clear()
+
+  #Header is printed and player lives and guesses are displayed
   print("********Logans Guessing Game********")
   print("Lives: {}".format(int_lives))
+  print("Current Guesses: {}".format(list_guess))
 
+  #Random number is generated
   n = random.randint(1, 10)
   print(n)
 
 
-
+  #While the player has more than 0 lives, run the game
   while int_lives > 0:
+
+    #Player takes first guess, guess is added to list
     guess = int(input("Guess Your number: "))
     list_guess.append(guess)
 
+    #If the user guesses right, the loop ends
     if guess == n:
 
-      print("You got it right!")
-      bool_playagain = False
-      break
-
-    elif guess != n:
       clear()
-      int_lives = int_lives - 1
-      print(int_lives)
-
       print("********Logans Guessing Game********")
       print("Lives: {}".format(int_lives))
       print("")
+
+      print("You got it right!")
+      print("")
+      print("The number was: {}".format(n))
+      print("You Guessed: {}".format(list_guess))
+      bool_playagain = False
+      input("DEBUG Press Enter to continue...")
+      break
+
+    #If the users guesses incorrectly, the loop continues
+    elif guess != n:
+      clear()
+      int_lives = int_lives - 1
+
+      print("********Logans Guessing Game********")
+      print("Lives: {}".format(int_lives))
+      print("Current Guesses: {}".format(list_guess))
+      print("")
       print("Incorrect!")
 
+  #End game text if the user runs out of lives
+  if int_lives == 0:
+    clear()
+    print("********Logans Guessing Game********")
+    print("Lives: {}".format(int_lives))
+    print("")
+    print("You lose, Your out of lives!")
+    print("")
+    print("The number was: {}".format(n))
+    print("You Guessed: {}".format(list_guess))
+    input("DEBUG Press Enter to continue...")
 
-  clear()
-  print("********Logans Guessing Game********")
-  print("Lives: {}".format(int_lives))
-  print("")
-  print("Your out of lives!")
-  print("The number was: {}".format(n))
-  
+
+'''needs 
+clear list if the users replays
+ask if the users wants to play again
+show the user their guesses
+play again option
+verify that inputs are the correct format and account for incorrect formats
+COMMENTING
+'''
